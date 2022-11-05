844cc4e43dfef652d76210f95b2160b7d0e55545
Rainer Kordmaa <asd@asd.com>
2022-11-05 21:26:18 +0200

    parser progress

--- src/xml_conf_parser/xml_conf_parser.c
+++ src/xml_conf_parser/xml_conf_parser.c
@@ -21,7 +21,7 @@
 #include "libxml/parser.h"
 
 /****************************************************************************/
-int ParseVendor(xmlNode *vendorinfo, long *vendorid){
+int ParseVendorId(xmlNode *vendorinfo, long *vendorid){
     //fprintf(stdout, "\t parsing vendorinfo\n");
     // first child is supposed to be <Id>, with vendor value, 2 for Beckhoff
     xmlNode *first_child, *node;
@@ -44,8 +44,74 @@ int ParseVendor(xmlNode *vendorinfo, long *vendorid){
 
     return res;
 }
+
+int ParseDeviceType(xmlNode *typedata, char *producttype, uint *productcode, uint *revisionNo){
+
+    return 0;
+}
+
 int ParseDescriptions(xmlNode *descriptions){
+    // data out
+    uint productcode, revisionNo;
+    char *producttype;
+
+    // parse
     fprintf(stdout, "\t parsing descriptions\n");
+    // <Descriptions><Devices><Device>the good stuff inside here</Device></Devices></Descriptions>
+    // one element of devices, inside one device, any deviation from that is not implemented
+    char expectedDevicesElementName[] = "Devices"; 
+    char expectedDeviceElementName[] = "Device"; 
+    char expectedTypeElementName[] = "Type"; 
+    char expectedNameElementName[] = "Name"; 
+    char expectedSmElementName[] = "Sm"; 
+    char expectedRxPDOElementName[] = "RxPdo"; 
+    char expectedTxPDOElementName[] = "TxPdo"; 
+    xmlNode *first_child, *node, *devices, *device;
+    devices = NULL;
+    device = NULL;
+    
+    // find devices
+    first_child = descriptions->children;
+    for (node = first_child; node; node = node->next){
+        if(strcmp(node->name, expectedDevicesElementName) == 0){
+            devices = node;
+            break;
+        }
+    }
+    if(devices == NULL){
+        fprintf(stdout, "\t <%s> not found\n", expectedDevicesElementName);
+        return -1;
+    }
+    
+    // find device
+    first_child = devices->children;
+    for (node = first_child; node; node = node->next){
+        if(strcmp(node->name, expectedDeviceElementName) == 0){
+            device = node;
+            break;
+        }
+    }
+    if(device == NULL){
+        fprintf(stdout, "\t <%s> not found\n", expectedDeviceElementName);
+        return -1;
+    }
+    // device has elements of interest - type, name, sm(don't know what that does, but it's probably important), RxPDO(outputs), TxPDO(inputs)
+    // find device
+    first_child = device->children;
+    for (node = first_child; node; node = node->next){
+        if(strcmp(node->name, expectedTypeElementName) == 0){
+            if(ParseDeviceType(node, producttype, &productcode, &revisionNo))return -1;
+        }/* else if(strcmp(node->name, expectedNameElementName) == 0) {
+            if(ParseDeviceName(node))return -1;
+        } else if(strcmp(node->name, expectedSmElementName) == 0) {
+            ; // what is Sm good for?
+        } else if(strcmp(node->name, expectedRxPDOElementName) == 0) {
+            if(ParseDeviceRxPDO(node))return -1;
+        } else if(strcmp(node->name, expectedTxPDOElementName) == 0) {
+            if(ParseDeviceTxPDO(node))return -1;
+        }*/
+    }
+
     return 0;
 }
 
@@ -60,13 +126,13 @@ int ParseSlave(xmlNode *slaveinfo){
     first_child = slaveinfo->children;
     for (node = first_child; node; node = node->next){
         if(strcmp(node->name, expectedVendorElementName) == 0){
-            if(ParseVendor(node, &vendorid))return -1;
+            if(ParseVendorId(node, &vendorid))return -1;
         } else if(strcmp(node->name, expectedDescriptionsElementName) == 0) {
             if(ParseDescriptions(node))return -1;
         }
     }
     
-    fprintf(stdout, "\t vendor id %i\n", vendorid);
+    fprintf(stdout, "\t vendor id %ld\n", vendorid);
 
     return 0;
 }
