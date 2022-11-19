aed5167fcbd40da120fb8629cdca929b6b7458c4
r2k <r2k@asd.asd>
2022-11-19 13:16:03 +0200

    renamed conf file for proper highlight

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir -p build
-cp -n conf/ethercatcfg.txt build/ethercatcfg.txt
+cp -n conf/ethercat.cfg build/ethercat.cfg
 cp -n conf/master_config.xml build/master_config.xml
 cmake -S . -B build/
 cmake --build build/
